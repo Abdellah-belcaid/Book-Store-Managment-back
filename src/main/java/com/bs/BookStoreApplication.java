124ba6cca96366288e43d2c55d1c4ef824ea1c3c
belcaid <belcaid.abdellah2001@gmail.com>
2023-09-05 11:39:43 +0200

    version 1

---
+++ src/main/java/com/bs/BookStoreApplication.java
@@ -0,0 +1,46 @@
+package com.bs;
+
+import com.bs.model.Role;
+import com.bs.model.User;
+import com.bs.repository.IUserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.LocalDateTime;
+
+
+@SpringBootApplication
+public class BookStoreApplication {
+    private static final Logger log = LoggerFactory.getLogger(BookStoreApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(IUserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            String adminEmail = "belcaid.abdellah2001@gmail.com";
+            String adminPassword = "admin12345";
+            if (userRepository.findByEmailIgnoreCase(adminEmail) == null) {
+                User adminUser = User.builder()
+                        .email(adminEmail)
+                        .role(Role.ADMIN)
+                        .createTime(LocalDateTime.now())
+                        .username("admin")
+                        .password(passwordEncoder.encode(adminPassword))
+                        .name("belcaid")
+                        .build();
+
+                userRepository.save(adminUser);
+                log.info("Admin user created. Username: " + adminUser.getUsername() + " - Password: " + adminPassword);
+            }
+        };
+    }
+
+}
---
+++ src/main/java/com/bs/repository/IUserRepository.java
@@ -0,0 +1,28 @@
+package com.bs.repository;
+
+import com.bs.model.Role;
+import com.bs.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface IUserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    @Modifying
+    @Query("update User set role = :role where username = :username")
+    void updateUserRole(@Param("username") String username, @Param("role") Role role);
+
+    boolean existsByUsername(String username);
+
+    User findByUsernameAndPassword(String username, String Password);
+
+    boolean existsByEmail(String email);
+
+    User findByEmailIgnoreCase(String email);
+
+
+}
