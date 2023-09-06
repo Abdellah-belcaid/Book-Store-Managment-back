6a73daca1f7b022375fd2baf159dbf108d89c680
belcaid <belcaid.abdellah2001@gmail.com>
2023-09-06 17:01:44 +0200

    version 1

--- src/main/java/com/bs/BookStoreApplication.java
+++ src/main/java/com/bs/BookStoreApplication.java
@@ -9,12 +9,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDateTime;
 
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class BookStoreApplication {
     private static final Logger log = LoggerFactory.getLogger(BookStoreApplication.class);
 
